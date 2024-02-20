f2e8f96f125474ad5cb0a47a638f4a746a3dda4a
svanmarc <stefaniavanmarck@gmail.com>
2024-02-20 15:37:23 +0100

    22

--- ex01/Fixed.cpp
+++ ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: svanmarc <@student.42perpignan.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 19:39:26 by svanmarc          #+#    #+#             */
-/*   Updated: 2024/02/20 14:42:02 by svanmarc         ###   ########.fr       */
+/*   Updated: 2024/02/20 15:30:41 by svanmarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,14 @@ Fixed::Fixed(const Fixed &src) : _value(src._value)
     std::cout << "Copy constructor called" << std::endl;
 }
 
+// here we shift the value to the left by 8 to get the fixed point value
 Fixed::Fixed(const int value) : _value(value << _fractionalBits)
 {
     std::cout << "Int constructor called" << std::endl;
 }
 
+// here we round the value to the nearest integer (roundf)
+// and then we shift the value to the left by 8 to get the fixed point value (value * 256)
 Fixed::Fixed(const float value) : _value(roundf(value * (1 << _fractionalBits)))
 {
     std::cout << "Float constructor called" << std::endl;
@@ -37,6 +40,8 @@ Fixed::~Fixed()
     std::cout << "Destructor called" << std::endl;
 }
 
+// here "this != &src" is to avoid self-assignment and to avoid deleting the object before copying it
+// if we delete the object before copying it, we will have a segfault
 Fixed &Fixed::operator=(const Fixed &src)
 {
     std::cout << "Copy assignment operator called" << std::endl;
@@ -57,14 +62,15 @@ void Fixed::setRawBits(int const raw)
 
 float Fixed::toFloat(void) const
 {
-    return (float)_value / (1 << _fractionalBits);
+    return (float)_value / (1 << _fractionalBits); // value / 256
 }
 
 int Fixed::toInt(void) const
 {
-    return _value >> _fractionalBits;
+    return _value >> _fractionalBits; // shift right to convert back to int
 }
 
+// here we overload the operator << to display the value of the object
 std::ostream &operator<<(std::ostream &out, const Fixed &fixed)
 {
     out << fixed.toFloat();
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,36 +6,36 @@
 /*   By: svanmarc <@student.42perpignan.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 19:23:32 by svanmarc          #+#    #+#             */
-/*   Updated: 2024/02/12 19:39:15 by svanmarc         ###   ########.fr       */
+/*   Updated: 2024/02/20 15:17:06 by svanmarc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FIXED_HPP
-# define FIXED_HPP
+#define FIXED_HPP
 
-# include <iostream>
-# include <cmath>
+#include <iostream>
+#include <cmath>
 
 class Fixed
 {
-    private:
-        int _value;
-        static const int _fractionalBits = 8;
+private:
+	int _value;
+	static const int _fractionalBits = 8;
 
-    public:
-		Fixed();
-		Fixed(const Fixed &fixed);
-		Fixed(const int value);
-		Fixed(const float value);
-		~Fixed();
+public:
+	Fixed();
+	Fixed(const Fixed &fixed);
+	Fixed(const int value);
+	Fixed(const float value);
+	~Fixed();
 
-		Fixed &operator=(const Fixed &fixed);
-		int getRawBits(void) const;
-		void setRawBits(int const raw);
-		float toFloat(void) const;
-		int toInt(void) const;
+	Fixed &operator=(const Fixed &fixed);
+	int getRawBits(void) const;
+	void setRawBits(int const raw);
+	float toFloat(void) const;
+	int toInt(void) const;
 };
 
 std::ostream &operator<<(std::ostream &out, const Fixed &fixed);
-
+// operator surcharge to display the value of the object
 #endif
